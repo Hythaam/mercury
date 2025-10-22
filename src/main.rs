bbe66100dca97dc7ecb48cfe1ec9ef738e65f2a3
Alex Hytha <hythaam@gmail.com>
2025-10-22 13:27:34 -0400

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,23 @@
-use poem::{listener::TcpListener, Route, Server, Middleware};
-use poem_openapi::{payload::Json, OpenApi, OpenApiService, Object};
+#[macro_use] extern crate rocket;
 
-mod plugins;//::http::HttpDestination;
-use plugins::http::HttpDestination;
+// mod plugins;//::http::HttpDestination;
+// use plugins::http::HttpDestination;
 
-mod data;
-use data::EventStore;
+// mod event_store;
+// use event_store::EventStore;
 
-struct EventStoreMiddleware {
-    event_store: EventStore,
-}
-impl<E: poem::Endpoint> Middleware<E> for EventStoreMiddleware {
-    type Output = EventStoreEndpoint<E>;
-
-    fn transform(&self, ep: E) -> Self::Output {
-        EventStoreEndpoint {
-            event_store: self.event_store.clone(),
-            inner: ep,
-        }
-    }
-}
-
-#[derive(Object)]
 struct EventScheduleRequest {
     event_body: String,
     schedule_time: i64,
     destination: String,//HttpDestination,
 }
 
-struct API;
-
-#[OpenApi]
-impl API {
-    #[oai(path = "/healthcheck", method = "get")]
-    async fn healthcheck(&self) -> Json<&'static str> {
-        // Eventually, this should provide real health status of dependent services.
-        Json("OK")
-    }
-
-    #[oai(path = "/schedule_event", method = "post")]
-    async fn schedule_event(&self, req: Json<EventScheduleRequest>) -> Json<&'static str> {
-        Json("Hello from Poem!")
-    }
+#[get("/")]
+fn index() -> &'static str {
+    "Hello World"
 }
 
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    let api_service = OpenApiService::new(API, "", "1.0").server("http://localhost:8000/api");
-    let ui = api_service.swagger_ui();
-
-    let event_store = EventStore::connect().await.expect("Failed to connect to the database");
-
-    let app = Route::new()
-        .at("/api", api_service)
-        .at("/docs", ui);
-    Server::new(TcpListener::bind("127.0.0.1:8000")).run(app).await
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
--- src/plugins/http/inbound.rs
+++ src/plugins/http/inbound.rs
@@ -1,1 +1,41 @@
+use poem::{
+  Endpoint, Middleware, Request, Result,
+};
+use crate::event_store::EventStore;
 
+struct EventStoreMiddleware<'a> {
+  store: &'a EventStore,
+}
+
+impl<'a> EventStoreMiddleware<'a> {
+  fn create(store: &'a EventStore) -> Self {
+    EventStoreMiddleware {
+      store: store
+    }
+  }
+}
+
+impl<'a, E: Endpoint> Middleware<E> for EventStoreMiddleware<'a> {
+  type Output = EventStoreMiddlewareImpl<'a, E>;
+  fn transform(&self, ep: E) -> Self::Output {
+    EventStoreMiddlewareImpl {
+      ep,
+      store: self.store
+    }
+  }
+}
+
+struct EventStoreMiddlewareImpl<E> {
+  ep: E,
+  store: &EventStore,
+}
+
+impl<E: Endpoint> Endpoint for EventStoreMiddlewareImpl<E> {
+  type Output = E::Output;
+  async fn call(&self, mut req: Request) -> Result<Self::Output> {
+
+    //req.extensions_mut().insert(self.store);
+
+    self.ep.call(req).await
+  }
+}
